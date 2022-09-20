c70f427249d3505e58466a0b2e67d7ab83366c88
radioactivecyy <104447515+radioactivecyy@users.noreply.github.com>
2022-09-20 22:32:01 +0800

    添加send时区分接收端和发送端的包类型

--- def.h
+++ def.h
@@ -8,6 +8,7 @@
 #define LIST_CONNECTER 4
 #define SEND 5
 #define EXIT 6
+#define SEND_BACK 7 //send要回包给发送客户端，用于区别发送客户端和接收客户端
 #define MAX_CONN 5//max connection
 
 // kbd ctrl
@@ -28,4 +29,3 @@ struct package{
 
 const char* menu_head="+--------------------+\n|       menu         |\n+--------------------+";
 #endif
-
